68954a021321966c2db978777c40f96d2fdd2e70
Kevin <kovacs.kevin98@gmail.com>
2020-07-11 14:22:16 +0200

    Move 1 image

--- src/main/java/com/kevin/imrez/Main.java
+++ src/main/java/com/kevin/imrez/Main.java
@@ -1,14 +1,30 @@
 package com.kevin.imrez;
 
-import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class Main {
     public static void main(String [] arg) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("folderLocation?");
-        String folderLocation = scanner.nextLine(); // scanner.findInLine ("pattern"); TODO
-        scanner.close();
-        System.out.println(folderLocation);
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("folderLocation?");
+//        String folderLocation = scanner.nextLine(); // scanner.findInLine ("pattern"); TODO
+//        scanner.close();
+//        System.out.println(folderLocation);
+
+        String fromPath = "C:\\Users\\Kevin\\Pictures\\april.jpg";
+        String toPath = "C:\\Users\\Kevin\\Desktop\\";
+
+        File file = new File(fromPath);
+
+        try {
+            Files.copy(file.toPath(), new File(toPath + file.getName()).toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
