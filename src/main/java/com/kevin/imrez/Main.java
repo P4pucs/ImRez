534c75a27918e855500baef6ec7010f04132fbea
P4pucs <kovacs.kevin98@gmail.com>
2020-07-17 18:24:18 +0200

    added config file for paths

--- src/main/java/com/kevin/imrez/Main.java
+++ src/main/java/com/kevin/imrez/Main.java
@@ -2,24 +2,37 @@ package com.kevin.imrez;
 
 import javax.imageio.ImageIO;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Properties;
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String [] arg) {
 
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("folderLocation?");
-//        String folderLocation = scanner.nextLine();
-//        scanner.close();
-//        System.out.println(folderLocation);
+        Properties p = new Properties();
+        try {
+            FileInputStream in = new FileInputStream("config.ini");
+            p.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        String fromPath = "C:\\Users\\Kevin\\Pictures\\";
-        String toPath = "C:\\Users\\Kevin\\Desktop\\Pictures\\";
+        String fromPath = p.get("fromPath").toString();
+        String toPath = p.get("toPath").toString();
 
         Pattern imagePattern = Pattern.compile("(.*)(.jpg|.png)", Pattern.CASE_INSENSITIVE);
         Matcher imageMatcher;
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("folderLocation?");
+//        fromPath = scanner.nextLine();
+//        scanner.close();
+//        System.out.println(toPath);
 
         File tmp = new File(fromPath);
         if(!(tmp.exists() || tmp.isDirectory())) {
@@ -30,7 +43,7 @@ public class Main {
         File IEAIAIO = new File(toPath);
         if(!(IEAIAIO.exists() || IEAIAIO.isDirectory())) {
             System.out.println("Directory not found...SO I MADE IT");
-            IEAIAIO.mkdir();
+            IEAIAIO.mkdirs();
         }
 
         File [] files = new File(fromPath).listFiles();
