1b19dc73a43eb5a3bbb573b3405967b85c112408
P4pucs <kovacs.kevin98@gmail.com>
2020-07-17 23:01:38 +0200

    changes to file managing

--- src/main/java/com/kevin/imrez/Main.java
+++ src/main/java/com/kevin/imrez/Main.java
@@ -3,7 +3,6 @@ package com.kevin.imrez;
 import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Properties;
@@ -13,64 +12,50 @@ import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String [] arg) {
-
-        Properties p = new Properties();
         try {
-            FileInputStream in = new FileInputStream("config.ini");
-            p.load(in);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            Properties config = new Properties();
 
-        String fromPath = p.get("fromPath").toString();
-        String toPath = p.get("toPath").toString();
+            config.load(new FileInputStream("config.ini"));
 
-        Pattern imagePattern = Pattern.compile("(.*)(.jpg|.png)", Pattern.CASE_INSENSITIVE);
-        Matcher imageMatcher;
+            String fromPath = config.get("fromPath").toString();
+            String toPath = config.get("toPath").toString();
 
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("folderLocation?");
-//        fromPath = scanner.nextLine();
-//        scanner.close();
-//        System.out.println(toPath);
+            Pattern imagePattern = Pattern.compile("(.*)(.jpg|.png)", Pattern.CASE_INSENSITIVE);
+            Matcher imageMatcher;
 
-        File tmp = new File(fromPath);
-        if(!(tmp.exists() || tmp.isDirectory())) {
-            System.out.println("filepath not found");
-            return;
-        }
+    //        Scanner scanner = new Scanner(System.in);
+    //        System.out.println("folderLocation?");
+    //        fromPath = scanner.nextLine();
+    //        scanner.close();
+    //        System.out.println(toPath);
 
-        File IEAIAIO = new File(toPath);
-        if(!(IEAIAIO.exists() || IEAIAIO.isDirectory())) {
-            System.out.println("Directory not found...SO I MADE IT");
-            IEAIAIO.mkdirs();
-        }
+            File tmp = new File(fromPath);
+            if(!(tmp.exists() && tmp.isDirectory())) {
+                System.out.println("filepath not found");
+                return;
+            }
 
-        File [] files = new File(fromPath).listFiles();
+            new File(toPath).mkdirs();
 
-        ArrayList<ImageData> images = new ArrayList<>();
+            File [] files = new File(fromPath).listFiles();
 
-        for (File file : files) {
-            imageMatcher = imagePattern.matcher(file.getName());
+            ArrayList<ImageData> images = new ArrayList<>();
 
-            if (file.isFile() && imageMatcher.matches()) {
-                try {
+            for (File file : files) {
+                imageMatcher = imagePattern.matcher(file.getName());
+
+                if (file.isFile() && imageMatcher.matches()) {
                     images.add(new ImageData(file.getName() ,ImageIO.read(file)));
-                } catch (Exception e) {
-                    e.printStackTrace();
                 }
             }
-        }
 
-        for(ImageData image : images) {
-            try {
+            for(ImageData image : images) {
                 ImageIO.write(image.scaledImage(), image.getType(), new File(toPath + image.getGUID() + "." + image.getType()));
-
                 System.out.println("name: " + toPath + image.getGUID() + "." +  image.getType());
-
-            } catch (Exception e) {
-                e.printStackTrace();
             }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
